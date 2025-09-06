a5276ede0f7828bda6e2195e2c354c2401b3a7bb
Harshvardhan <shreeharshpb11@gmail.com>
2025-09-06 23:51:05 +0530

    completed navbar UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div className="text-xl">App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,14 +5,19 @@ const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(true);
   const [isSeller, setIsSeller] = useState(false);
-  const value = { navigate, user, setUser, isSeller, setIsSeller };
-  return (
-    <AppContext.Provider value={value}>
-      {children}
-    </AppContext.Provider>
-  );
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export const useAppContext = () => {

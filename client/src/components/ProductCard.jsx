2e47f8b6230e72af52918b90d7662bb3875014d5
Harshvardhan <shreeharshpb11@gmail.com>
2025-09-11 00:46:10 +0530

    complete productcatergory page UI

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
 
 const ProductCard = ({ products }) => {
-  const [count, setCount] = useState(0);
   const { currency, addToCart, removeFromCart, cartItems, navigate } =
     useAppContext();
-  console.log(count);
   return (
     products && (
-      <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
+      <div
+        className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full"
+        onClick={() => {
+          navigate(
+            `/products/${products.category.toLowerCase()}/${products._id}`
+          );
+          scrollTo(0, 0);
+        }}
+      >
         <div className="group cursor-pointer flex items-center justify-center px-2">
           <img
             className="group-hover:scale-105 transition max-w-26 md:max-w-36"
